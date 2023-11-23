c846e024cc8dc1ee4b7aa87956fbc93d24ad8a0c
Electrostats <Super_justy@hotmail.com>
2023-11-22 21:46:30 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,84 @@
+﻿using BaboonAPI.Hooks.Initializer;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.IO;
+using TootTallyCore.Utils.TootTallyModules;
+using TootTallySettings;
+using UnityEngine;
+
+namespace TootTally.ModuleTemplate
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("TootTallyCore", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("TootTallySettings", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BaseUnityPlugin, ITootTallyModule
+    {
+        public static Plugin Instance;
+
+        private const string CONFIG_NAME = "ModuleTemplate.cfg";
+        private Harmony _harmony;
+        public ConfigEntry<bool> ModuleConfigEnabled { get; set; }
+        public bool IsConfigInitialized { get; set; }
+
+        //Change this name to whatever you want
+        public string Name { get => PluginInfo.PLUGIN_NAME; set => Name = value; }
+
+        public static TootTallySettingPage settingPage;
+
+        public static void LogInfo(string msg) => Instance.Logger.LogInfo(msg);
+        public static void LogError(string msg) => Instance.Logger.LogError(msg);
+
+        private void Awake()
+        {
+            if (Instance != null) return;
+            Instance = this;
+            _harmony = new Harmony(Info.Metadata.GUID);
+
+            GameInitializationEvent.Register(Info, TryInitialize);
+        }
+
+        private void TryInitialize()
+        {
+            // Bind to the TTModules Config for TootTally
+            ModuleConfigEnabled = TootTallyCore.Plugin.Instance.Config.Bind("Modules", "<insert module name here>", true, "<insert module description here>");
+            TootTallyModuleManager.AddModule(this);
+            TootTallySettings.Plugin.Instance.AddModuleToSettingPage(this);
+        }
+
+        public void LoadModule()
+        {
+            string configPath = Path.Combine(Paths.BepInExRootPath, "config/");
+            ConfigFile config = new ConfigFile(configPath + CONFIG_NAME, true) { SaveOnConfigSet = true };
+            // Set your config here by binding them to the related ConfigEntry
+            // Example:
+            // Unlimited = config.Bind(CONFIG_FIELD, "Unlimited", DEFAULT_UNLISETTING)
+
+            settingPage = TootTallySettingsManager.AddNewPage("ModulePageName", "HeaderText", 40f, new Color(0,0,0,0));
+            if (settingPage != null) {
+                // Use TootTallySettingPage functions to add your objects to TootTallySetting
+                // Example:
+                // page.AddToggle(name, option.Unlimited);
+            }
+
+            _harmony.PatchAll(typeof(ModuleTemplatePatches));
+            LogInfo($"Module loaded!");
+        }
+
+        public void UnloadModule()
+        {
+            _harmony.UnpatchSelf();
+            settingPage.Remove();
+            LogInfo($"Module unloaded!");
+        }
+
+        public static class ModuleTemplatePatches
+        {
+            // Apply your Trombone Champ patches here
+        }
+
+        // Add your ConfigEntry objects that define your configs
+        // Example:
+        // public ConfigEntry<bool> Unlimited { get; set; }
+    }
+}
